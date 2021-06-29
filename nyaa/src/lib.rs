9d9c8bfc80d0197cfb2f2971d632c2f9b245bd1e
Stefan Breetveld <spiderbiggen@gmail.com>
2021-06-29 15:29:22 +0200

    improve nyaa lib

--- nyaa/src/lib.rs
+++ nyaa/src/lib.rs
@@ -18,14 +18,18 @@ const TRACKERS: &[&str] = &[
     "udp://open.stealth.si:80/announce",
     "udp://tracker.opentrackr.org:1337/announce",
     "udp://exodus.desync.com:6969/announce",
-    "udp://tracker.torrent.eu.org:451/announce"
+    "udp://tracker.torrent.eu.org:451/announce",
 ];
+const BASE_URL: &str = "https://nyaa.si/?page=rss";
 
 lazy_static! {
-    static ref BASE_URL: Url = Url::parse("https://nyaa.si/?page=rss").unwrap();
-    static ref SOURCES: Vec<AnimeSource> = vec![
-        AnimeSource::new("[SubsPlease]", Some("1_2"), "^\\[.*?] (.*) - (\\d+)(?:\\.(\\d+))?(?:[vV](\\d+?))? \\((\\d+?p)\\) \\[.*?\\].mkv", None, vec!["(1080p)", "(720p)", "(540p)", "(480p)"]),
-    ];
+    static ref SOURCES: Vec<AnimeSource> = vec![AnimeSource::new(
+        "[SubsPlease]",
+        Some("1_2"),
+        "^\\[.*?] (.*) - (\\d+)(?:\\.(\\d+))?(?:[vV](\\d+?))? \\((\\d+?p)\\) \\[.*?\\].mkv",
+        None,
+        vec!["(1080p)", "(720p)", "(540p)", "(480p)"]
+    ),];
 }
 
 #[derive(Clone, Debug)]
@@ -38,7 +42,16 @@ pub struct AnimeSource {
 }
 
 impl AnimeSource {
-    fn new<K>(key: K, category: Option<K>, regex: K, filter: Option<K>, resolutions: Vec<K>) -> AnimeSource where K: Into<String> {
+    fn new<K>(
+        key: K,
+        category: Option<K>,
+        regex: K,
+        filter: Option<K>,
+        resolutions: Vec<K>,
+    ) -> AnimeSource
+    where
+        K: Into<String>,
+    {
         AnimeSource {
             key: key.into(),
             category: category.and_then(|c| Some(c.into())),
@@ -69,7 +82,6 @@ pub struct Anime {
     pub pub_date: DateTime<FixedOffset>,
 }
 
-
 impl Anime {
     pub fn get_magnet(&self) -> Option<String> {
         self.id.as_deref().and_then(|hash| {
@@ -92,17 +104,16 @@ pub async fn get_anime() -> Vec<Anime> {
     let client = Client::new();
     for source in SOURCES.clone() {
         let len = source.resolutions.len();
-        for i in 0..max(1, len) {
-            let url = build_url(&source, i);
-            let client = client.clone();
-            let handle = tokio::spawn(get_anime_for(client, url, source.clone()));
-            tasks.push(handle);
-        }
+        (0..max(1, len))
+            .filter_map(|i| build_url(&source, i))
+            .map(|url| tokio::spawn(get_anime_for(client.clone(), url, source.clone())))
+            .for_each(|handle| tasks.push(handle));
     }
     let joined = future::join_all(tasks).await;
-    joined.into_iter()
-        .flat_map(|item| item.ok())
-        .flatten()
+    joined
+        .into_iter()
+        .filter(Result::is_ok)
+        .flat_map(|item| item.unwrap())
         .collect()
 }
 
@@ -113,29 +124,26 @@ async fn get_anime_for(client: Client, url: Url, source: AnimeSource) -> Vec<Ani
     return map_anime(val.items, &source);
 }
 
-fn build_url(provider: &AnimeSource, res_index: usize) -> Url {
-    let mut url = BASE_URL.clone();
-    let mut pairs = url.query_pairs_mut();
+fn build_url(provider: &AnimeSource, res_index: usize) -> Option<Url> {
+    let mut filters: Vec<(&str, &str)> = Vec::new();
+
     let mut query: String = provider.key.to_string();
     if let Some(res) = provider.resolutions.get(res_index) {
         query.push(' ');
         query.push_str(res);
     }
-    pairs.append_pair("q", &query);
+    filters.push(("q", &query));
     if let Some(ref category) = provider.category {
-        pairs.append_pair("c", category.as_str());
+        filters.push(("c", category.as_str()));
     }
     if let Some(ref filter) = provider.filter {
-        pairs.append_pair("f", filter.as_str());
+        filters.push(("f", filter.as_str()));
     }
-    drop(pairs);
-    url.to_owned()
+    Url::parse_with_params(BASE_URL, filters).ok()
 }
 
 async fn get_feed(client: Client, url: &Url) -> Result<Channel, Box<dyn Error>> {
-    let content = client.get(url.as_str())
-        .send().await?
-        .bytes().await?;
+    let content = client.get(url.as_str()).send().await?.bytes().await?;
     let channel = Channel::read_from(&content[..])?;
     Ok(channel)
 }
@@ -143,7 +151,8 @@ async fn get_feed(client: Client, url: &Url) -> Result<Channel, Box<dyn Error>> 
 fn map_anime(items: Vec<Item>, source: &AnimeSource) -> Vec<Anime> {
     Regex::new(source.regex.as_str())
         .map(|regex| {
-            items.into_iter()
+            items
+                .into_iter()
                 .filter_map(move |i| to_anime(i, &regex))
                 .collect()
         })
@@ -151,10 +160,20 @@ fn map_anime(items: Vec<Item>, source: &AnimeSource) -> Vec<Anime> {
 }
 
 #[derive(Debug, Eq, PartialEq)]
-struct AnimeComponents(String, String, String, Option<i32>, Option<i32>, Option<i32>);
+struct AnimeComponents(
+    String,
+    String,
+    String,
+    Option<i32>,
+    Option<i32>,
+    Option<i32>,
+);
 
 impl AnimeComponents {
-    fn from_string<S>(inp: Option<S>, regex: &Regex) -> Option<AnimeComponents> where S: Into<String> {
+    fn from_string<S>(inp: Option<S>, regex: &Regex) -> Option<AnimeComponents>
+    where
+        S: Into<String>,
+    {
         inp.and_then(|s| Some(s.into()))
             .as_ref()
             .and_then(|title| regex.captures(title))
@@ -164,22 +183,34 @@ impl AnimeComponents {
                 let version: Option<i32> = cap.get(4).and_then(|a| a.as_str().parse::<i32>().ok());
                 let resolution: String = cap.get(5).unwrap().as_str().to_string();
 
-                Some(AnimeComponents(cap[0].into(), cap[1].into(), resolution, episode, decimal, version))
+                Some(AnimeComponents(
+                    cap[0].into(),
+                    cap[1].into(),
+                    resolution,
+                    episode,
+                    decimal,
+                    version,
+                ))
             })
     }
 }
 
 fn to_anime(item: Item, regex: &Regex) -> Option<Anime> {
-    let date = item.pub_date.as_ref().and_then(|str| DateTime::parse_from_rfc2822(str).ok())?;
+    let date = item
+        .pub_date
+        .as_ref()
+        .and_then(|str| DateTime::parse_from_rfc2822(str).ok())?;
     let link = item.link?;
     let comments: String = item.guid?.value;
-    let id: Option<String> = item.extensions.get("nyaa")
+    let id: Option<String> = item
+        .extensions
+        .get("nyaa")
         .and_then(|nyaa| nyaa.get("infoHash"))
         .and_then(|info_hash| info_hash.deref().get(0))
         .and_then(|extension| extension.value.clone());
 
-    AnimeComponents::from_string(item.title, regex)
-        .and_then(|AnimeComponents(file_name, title, resolution, episode, decimal, version)| {
+    AnimeComponents::from_string(item.title, regex).and_then(
+        |AnimeComponents(file_name, title, resolution, episode, decimal, version)| {
             Some(Anime {
                 id,
                 episode,
@@ -192,9 +223,9 @@ fn to_anime(item: Item, regex: &Regex) -> Option<Anime> {
                 torrent: link,
                 pub_date: date,
             })
-        })
+        },
+    )
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -203,7 +234,14 @@ mod tests {
     #[test]
     fn test_parse_anime_components_basic() {
         let input = "[_] Test Anime - 01 (1080p) [_].mkv";
-        let expected = AnimeComponents("[_] Test Anime - 01 (1080p) [_].mkv".into(), "Test Anime".into(), "1080p".into(), Some(1), None, None);
+        let expected = AnimeComponents(
+            "[_] Test Anime - 01 (1080p) [_].mkv".into(),
+            "Test Anime".into(),
+            "1080p".into(),
+            Some(1),
+            None,
+            None,
+        );
         let source = SOURCES.get(0).unwrap();
         let regex = Regex::new(&source.regex).unwrap();
         let result = AnimeComponents::from_string(Some(input), &regex);
@@ -214,7 +252,14 @@ mod tests {
     #[test]
     fn test_parse_anime_components_with_version_lower() {
         let input = "[_] Test Anime - 01v1 (1080p) [_].mkv";
-        let expected = AnimeComponents(input.into(), "Test Anime".into(), "1080p".into(), Some(1), None, Some(1));
+        let expected = AnimeComponents(
+            input.into(),
+            "Test Anime".into(),
+            "1080p".into(),
+            Some(1),
+            None,
+            Some(1),
+        );
         let source = SOURCES.get(0).unwrap();
         let regex = Regex::new(&source.regex).unwrap();
         let result = AnimeComponents::from_string(Some(input), &regex);
@@ -225,7 +270,14 @@ mod tests {
     #[test]
     fn test_parse_anime_components_with_version_upper() {
         let input = "[_] Test Anime - 01V1 (1080p) [_].mkv";
-        let expected = AnimeComponents(input.into(), "Test Anime".into(), "1080p".into(), Some(1), None, Some(1));
+        let expected = AnimeComponents(
+            input.into(),
+            "Test Anime".into(),
+            "1080p".into(),
+            Some(1),
+            None,
+            Some(1),
+        );
         let source = SOURCES.get(0).unwrap();
         let regex = Regex::new(&source.regex).unwrap();
         let result = AnimeComponents::from_string(Some(input), &regex);
@@ -236,7 +288,14 @@ mod tests {
     #[test]
     fn test_parse_anime_components_with_decimal() {
         let input = "[_] Test Anime - 01.1 (1080p) [_].mkv";
-        let expected = AnimeComponents(input.into(), "Test Anime".into(), "1080p".into(), Some(1), Some(1), None);
+        let expected = AnimeComponents(
+            input.into(),
+            "Test Anime".into(),
+            "1080p".into(),
+            Some(1),
+            Some(1),
+            None,
+        );
         let source = SOURCES.get(0).unwrap();
         let regex = Regex::new(&source.regex).unwrap();
         let result = AnimeComponents::from_string(Some(input), &regex);
@@ -247,7 +306,14 @@ mod tests {
     #[test]
     fn test_parse_anime_components_with_decimal_and_version() {
         let input = "[_] Test Anime - 01.1V1 (1080p) [_].mkv";
-        let expected = AnimeComponents(input.into(), "Test Anime".into(), "1080p".into(), Some(1), Some(1), Some(1));
+        let expected = AnimeComponents(
+            input.into(),
+            "Test Anime".into(),
+            "1080p".into(),
+            Some(1),
+            Some(1),
+            Some(1),
+        );
         let source = SOURCES.get(0).unwrap();
         let regex = Regex::new(&source.regex).unwrap();
         let result = AnimeComponents::from_string(Some(input), &regex);
@@ -258,7 +324,14 @@ mod tests {
     #[test]
     fn test_parse_anime_components_with_dash_in_title() {
         let input = "[_] Test-Anime - 01.1V1 (1080p) [_].mkv";
-        let expected = AnimeComponents(input.into(), "Test-Anime".into(), "1080p".into(), Some(1), Some(1), Some(1));
+        let expected = AnimeComponents(
+            input.into(),
+            "Test-Anime".into(),
+            "1080p".into(),
+            Some(1),
+            Some(1),
+            Some(1),
+        );
         let source = SOURCES.get(0).unwrap();
         let regex = Regex::new(&source.regex).unwrap();
         let result = AnimeComponents::from_string(Some(input), &regex);
