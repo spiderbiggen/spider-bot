8e740880d41c513b9fba8c216bc076b467122940
Stefan Breetveld <spiderbiggen@gmail.com>
2021-06-29 15:28:26 +0200

    improve kitsu lib

--- kitsu/src/lib.rs
+++ kitsu/src/lib.rs
@@ -3,77 +3,67 @@ extern crate jsonapi;
 #[macro_use]
 extern crate serde;
 
+use thiserror::Error as ThisError;
 
-pub mod error {
-    use std::{error, fmt};
-    use std::fmt::Formatter;
-    use jsonapi::api::DocumentError;
-
-    #[derive(Debug)]
-    pub enum Error {
-        Empty,
-        Api(DocumentError),
-        Parse(jsonapi::errors::Error),
-    }
-
-    impl fmt::Display for Error {
-        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-            match *self {
-                Error::Empty =>
-                    write!(f, "Empty response"),
-                Error::Api(..) =>
-                    write!(f, "Wrong Response from api"),
-                // The wrapped error contains additional information and is available
-                // via the source() method.
-                Error::Parse(..) =>
-                    write!(f, "Serde failed somewhere"),
-            }
-        }
-    }
-
-    impl error::Error for Error {}
+#[derive(Debug, ThisError)]
+pub enum Error {
+    #[error("Empty Response")]
+    Empty,
+    #[error("Wrong Response from api")]
+    Api(jsonapi::api::DocumentError),
+    #[error(transparent)]
+    Parse(#[from] jsonapi::errors::Error),
+    #[error(transparent)]
+    Request(#[from] reqwest::Error),
+    #[error(transparent)]
+    Url(#[from] url::ParseError),
 }
 
-pub mod models {
-    use std::error::Error as Err;
+pub type Result<T> = std::result::Result<T, Error>;
 
+
+pub mod models {
     use chrono::{DateTime, Utc};
     use jsonapi::api::*;
     use jsonapi::model::*;
 
-    use super::error::Error;
+    use crate::{Error, Result};
+
 
     pub trait ParseJsonApi: JsonApiModel {
-        fn from_document(document: JsonApiDocument) -> Result<Self, Box<dyn Err>> {
+        fn from_document(document: JsonApiDocument) -> Result<Self> {
             match document {
-                JsonApiDocument::Data(doc) => Self::from_jsonapi_document(&doc).map_err(|err| Error::Parse(err).into()),
+                JsonApiDocument::Data(doc) => Self::from_jsonapi_document(&doc).map_err(|err| Error::Parse(err)),
                 JsonApiDocument::Error(err) => Err(Error::Api(err).into()),
             }
         }
 
-        fn collection_from_document(document: JsonApiDocument) -> Result<Vec<Self>, Box<dyn Err>> {
+        fn collection_from_document(document: JsonApiDocument) -> Result<Vec<Self>> {
             match document {
                 JsonApiDocument::Data(doc) => match doc.data {
                     Some(data) => match data {
                         PrimaryData::None => Ok(Vec::new()),
-                        PrimaryData::Single(resource) =>
+                        PrimaryData::Single(resource) => {
                             Self::from_jsonapi_resource(&resource, &None)
                                 .map(|a| vec![a])
-                                .map_err(|err| Error::Parse(err).into()),
-                        PrimaryData::Multiple(resources) => {
-                            resources.iter()
-                                .map(|res| Self::from_jsonapi_resource(&res, &None).map_err(|err| Error::Parse(err).into()))
-                                .collect()
+                                .map_err(|err| Error::Parse(err).into())
                         }
+                        PrimaryData::Multiple(resources) => resources
+                            .iter()
+                            .map(|res| {
+                                Self::from_jsonapi_resource(&res, &None)
+                                    .map_err(|err| Error::Parse(err).into())
+                            })
+                            .collect(),
                     },
-                    None => Err(Error::Empty.into())
+                    None => Err(Error::Empty.into()),
                 },
-                JsonApiDocument::Error(err) => Err(Error::Api(err).into())
+                JsonApiDocument::Error(err) => Err(Error::Api(err).into()),
             }
         }
     }
 
-    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
     pub struct ImageSet {
         #[serde(skip_serializing_if = "Option::is_none")]
         pub tiny: Option<String>,
@@ -87,7 +77,7 @@ pub mod models {
         pub original: Option<String>,
     }
 
-    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
     pub struct Anime {
         pub id: String,
         #[serde(rename = "createdAt")]
@@ -95,15 +85,18 @@ pub mod models {
         #[serde(rename = "updatedAt")]
         pub updated_at: DateTime<Utc>,
         pub slug: String,
-        pub synopsis: String,
-        pub description: String,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub synopsis: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub description: Option<String>,
         pub titles: HashMap<String, String>,
         #[serde(rename = "canonicalTitle")]
         pub canonical_title: String,
         #[serde(rename = "abbreviatedTitles")]
         pub abbreviated_titles: Vec<String>,
         #[serde(rename = "averageRating")]
-        pub rating: String,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub rating: Option<String>,
         #[serde(rename = "showType")]
         pub show_type: String,
         #[serde(rename = "subType")]
@@ -116,23 +109,21 @@ pub mod models {
         #[serde(skip_serializing_if = "Option::is_none")]
         pub cover_image: Option<ImageSet>,
         #[serde(rename = "episodeCount")]
-        pub episode_count: i32,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub episode_count: Option<i32>,
     }
 
     jsonapi_model!(Anime; "anime");
     impl ParseJsonApi for Anime {}
 }
 
-
 pub mod api {
-    use std::error::Error as Err;
-
     use jsonapi::api::*;
-    use jsonapi::model::*;
     use reqwest;
     use url::Url;
 
-    use super::error::Error;
+    use crate::{Error, Result};
+
     use super::models::ParseJsonApi;
 
     const JSON_API_TYPE: &str = "application/vnd.api+json";
@@ -146,46 +137,40 @@ pub mod api {
             .header(CONTENT_TYPE_HEADER, JSON_API_TYPE)
     }
 
-    async fn get_document(url: Url) -> Result<JsonApiDocument, Box<dyn Err>> {
-        let document: JsonApiDocument = get_url_builder(url)
-            .send().await?
-            .json().await?;
+    async fn get_document(url: Url) -> Result<JsonApiDocument> {
+        let document: JsonApiDocument = get_url_builder(url).send().await?.json().await?;
         return Ok(document);
     }
 
-    pub(self) async fn get_resource<T: ParseJsonApi>(url: Url) -> Result<T, Box<dyn Err>> {
+    pub(self) async fn get_resource<T: ParseJsonApi>(url: Url) -> Result<T> {
         let doc = get_document(url).await?;
         T::from_document(doc)
     }
 
-    pub(self) async fn get_resources<T: ParseJsonApi>(url: Url) -> Result<Vec<T>, Box<dyn Err>> {
+    pub(self) async fn get_resources<T: ParseJsonApi>(url: Url) -> Result<Vec<T>> {
         let doc = get_document(url).await?;
         T::collection_from_document(doc)
     }
 
     pub mod anime {
-        use crate as kitsu;
-        use std::error::Error;
-
         use jsonapi::model::HashMap;
         use jsonapi::query::{PageParams, Query};
         use url::Url;
 
-        use kitsu::api;
-        use kitsu::models;
+        use crate::{api, models, Error, Result};
 
-        pub async fn get_resource(id: u64) -> Result<models::Anime, Box<dyn Error>> {
+        pub async fn get_resource(id: u64) -> Result<models::Anime> {
             let url_string = format!("https://kitsu.io/api/edge/anime/{}", id);
             let url = Url::parse(&url_string)?;
             let anime = api::get_resource::<models::Anime>(url).await?;
             return Ok(anime);
         }
 
-        pub async fn get_collection() -> Result<Vec<models::Anime>, Box<dyn Error>> {
+        pub async fn get_collection<S: AsRef<str>>(title: S) -> Result<Vec<models::Anime>> {
             let url_string = "https://kitsu.io/api/edge/anime";
             let mut url = Url::parse(url_string)?;
             let mut map = HashMap::new();
-            map.insert("text".to_string(), vec!["Boku no hero academia".to_string()]);
+            map.insert("text".to_string(), vec![title.as_ref().into()]);
             let query = Query {
                 sort: None,
                 _type: "anime".to_string(),
@@ -196,10 +181,8 @@ pub mod api {
                 filter: Some(map),
                 fields: None,
                 include: None,
-            };
-            let params = query.to_params();
-            url.set_query(Some(&params));
-            println!("{}", url.to_string());
+            }.to_params();
+            url.set_query(Some(&query));
             let anime = api::get_resources::<models::Anime>(url).await?;
             return Ok(anime);
         }
